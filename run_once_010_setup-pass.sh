6fc628a77de879d5b2be9e5ec8e57c7c8cee1426
Shinigami072 <krzys.stasiowski@gmail.com>
2022-01-09 21:47:29 +0100

    Fix Env Varialbles not avaliable for script:wq

--- run_once_010_setup-pass.sh
+++ run_once_010_setup-pass.sh
@@ -1,4 +1,7 @@
 #!/bin/env bash
+export GPG_TTY="$(tty)"
+export SSH_AUTH_SOCK="/run/user/$UID/gnupg/S.gpg-agent.ssh"
+gpg-connect-agent updatestartuptty /bye > /dev/null
 
 if [ -e ~/.password-store/.git ]; then
 	pass git pull
