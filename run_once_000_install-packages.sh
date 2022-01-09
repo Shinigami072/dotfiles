6ffd3e3f525c3892d26369b984605c27766daf5a
Shinigami072 <krzys.stasiowski@gmail.com>
2022-01-09 20:59:14 +0100

    Fix backticks in install script

--- run_once_000_install-packages.sh
+++ run_once_000_install-packages.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 # CONSTANTS
-RED=`\033[0;31m`
-GREEN=`\033[0;32m`
-YELLOW=`\033[1;33m`
-NC=`\033[0m`
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
 
 
 # Pre Setup
